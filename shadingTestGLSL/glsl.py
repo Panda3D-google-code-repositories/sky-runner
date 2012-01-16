beaa2c53bd31702be68bc55035ee705205309e77
fmc <felipe@libra>
2012-01-16 16:03:29 -0200

    shading Test

---
+++ shadingTestGLSL/glsl.py
@@ -0,0 +1,21 @@
+import sys 
+from direct.showbase.ShowBase import ShowBase 
+from panda3d.core import Shader 
+
+class TestApp(ShowBase): 
+    def __init__(self): 
+        ShowBase.__init__(self) 
+        
+        self.accept("escape", sys.exit) 
+        
+        node = self.loader.loadModel('rawCube') 
+        node.reparentTo(self.render) 
+        
+        shader = Shader.load(Shader.SLGLSL, 'simple.vert', 'simple.frag') 
+        node.setShader(shader) 
+        
+        base.disableMouse() 
+        self.camera.setPos(0, -10, 0) 
+        
+app = TestApp() 
+app.run()
