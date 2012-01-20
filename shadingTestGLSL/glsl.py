8a3965c0bdd51aa286791dbec8bc4508aae74b83
fmc <felipe@libra>
2012-01-20 17:29:11 -0200

    Alpha ok self.cube.setTransparency(TransparencyAttrib.MAlpha)

--- shadingTestGLSL/glsl.py
+++ shadingTestGLSL/glsl.py
@@ -1,22 +1,52 @@
 import sys 
+
+#Shaders
+from pandac.PandaModules import loadPrcFileData
+
 from direct.showbase.ShowBase import ShowBase 
-from panda3d.core import Shader 
+from panda3d.core import Shader, Vec4 ,AntialiasAttrib , TransparencyAttrib , VBase3
+
+loadPrcFileData("", "framebuffer-multisample 1")
 
 class TestApp(ShowBase): 
     def __init__(self): 
         ShowBase.__init__(self) 
-        
+
+        render.setAntialias(AntialiasAttrib.MMultisample,1)        
         self.accept("escape", sys.exit) 
         
-        node = self.loader.loadModel('rawCube') 
-        node.reparentTo(self.render) 
         
-        shader = Shader.load(Shader.SLGLSL, 'simple.vert', 'simple.frag') 
-        node.setShader(shader) 
+        #node = self.loader.loadModel('../blender/sky/box-2.49.egg') 
+        self.cube = self.loader.loadModel('paralele-2.49.egg')
+        self.cube.reparentTo(self.render)
+        self.cube.setTransparency(TransparencyAttrib.MAlpha) 
         
+                #node = self.loader.loadModel('../blender/sky/box-2.49.egg') 
+        self.cube2 = self.loader.loadModel('paralele-2.49.egg')
+        self.cube2.reparentTo(self.render)
+        self.cube2.setTransparency(TransparencyAttrib.MAlpha) 
+        
+        self.cube.setPos(0.0,0.0,5.0)
+        
+        sky = self.loader.loadModel('../blender/sky/skydomeblendSmooth.egg')
+        sky.reparentTo(self.render)
+        
+        
+        shader = Shader.load(Shader.SLGLSL, 'glass.vert', 'glass.frag') 
+        self.cube.setShader(shader) 
+                
+        self.stepTask = taskMgr.add(self.timer, "timer")
+        self.stepTask.last = 0    
+                
 #        base.disableMouse() 
-#        self.camera.setPos(0, -10, 0) 
-        base.trackball.node().setPos(0, 10, 0)
+ 
+        base.trackball.node().setPos(0, 5, 0)
+        
+    def timer(self, task):
+        
+        self.cube.setShaderInput("LightPosition", VBase3(base.trackball.node().getHpr()))
+        
+        return task.cont        
         
 app = TestApp() 
 app.run()
